e6182691226c04ed8805c66d71ee34e49f621761
Vadim <maslovvadim398@gmail.com>
2023-03-20 15:47:45 +0300

    change image add method

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,32 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.currentCompositionLocalContext
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             LearnTogetherTheme {
 
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Image(bitmap = ImageBitmap.imageResource(R.drawable.bg_compose_background),
-                        contentDescription = "Head img of the app" )
-                    Greeting("Android")
+//                    Image(bitmap = ImageBitmap.imageResource(R.drawable.bg_compose_background),
+//                        contentDescription = "Head img of the app", alignment = Alignment.TopStart  )
+                    Image(painter = painterResource(id = R.drawable.bg_compose_background) ,
+                        contentDescription = "Head",
+
+                        )
+//                    Greeting("Android Vedroid")
+                    Text("Jetpack Compose Tutorial")
                 }
+
+
             }
         }
     }
