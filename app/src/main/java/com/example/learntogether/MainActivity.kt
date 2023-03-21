bbd40cc41980d2d0f934fcfb8f872b4dff6a9f3e
Vadim <maslovvadim398@gmail.com>
2023-03-21 10:15:28 +0300

    Places img, now true

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -4,19 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.learntogether.ui.theme.LearnTogetherTheme
@@ -37,7 +31,7 @@ class MainActivity : ComponentActivity() {
 //                    Image(bitmap = ImageBitmap.imageResource(R.drawable.bg_compose_background),
 //                        contentDescription = "Head img of the app", alignment = Alignment.TopStart  )
                     Image(painter = painterResource(id = R.drawable.bg_compose_background) ,
-                        contentDescription = "Head",
+                        contentDescription = "Head", alignment = Alignment.TopStart
 //
                         )
 //                    Greeting("Android Vedroid")
