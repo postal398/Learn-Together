9f4b79ba969884924c5370badf4fe6e8918b3c55
Vadim <maslovvadim398@gmail.com>
2023-03-21 17:16:48 +0300

    Article task completed

--- app/src/main/java/com/example/learntogether/MainActivity.kt
+++ app/src/main/java/com/example/learntogether/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.learntogether
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -12,7 +15,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.learntogether.ui.theme.LearnTogetherTheme
 
 class MainActivity : ComponentActivity() {
@@ -28,14 +35,29 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-//                    Image(bitmap = ImageBitmap.imageResource(R.drawable.bg_compose_background),
-//                        contentDescription = "Head img of the app", alignment = Alignment.TopStart  )
-                    Image(painter = painterResource(id = R.drawable.bg_compose_background) ,
-                        contentDescription = "Head", alignment = Alignment.TopStart
-//
+                    Column {
+                        Image(
+                            painter = painterResource(id = R.drawable.bg_compose_background),
+                            contentDescription = "Head", alignment = Alignment.TopStart,
                         )
-//                    Greeting("Android Vedroid")
-                    Text("Jetpack Compose Tutorial")
+                        Text( //first Text
+                            "Jetpack Compose Tutorial",
+                            fontSize = 24.sp,
+                            modifier = Modifier.padding(16.dp)
+                        )
+                        Text(//second Text
+                            "Jetpack Compose is a modern toolkit for building native Android UI. Compose simplifies and accelerates UI development on Android with less code, powerful tools, and intuitive Kotlin APIs.",
+                            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+                            textAlign = TextAlign.Justify
+                        )
+                        Text(//third Text
+                            " In this tutorial, you build a simple UI component with declarative functions. You call Compose functions to say what elements you want and the Compose compiler does the rest. Compose is built around Composable functions. These functions let you define your app's UI programmatically because they let you describe how it should look and provide data dependencies, rather than focus on the process of the UI's construction, such as initializing an element and then attaching it to a parent. To create a Composable function, you add the @Composable annotation to the function name.",
+                            modifier = Modifier.padding(16.dp),
+                            textAlign = TextAlign.Justify
+                            )
+
+                    }
+
                 }
 
 
@@ -44,15 +66,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    LearnTogetherTheme {
-        Greeting("Android")
-    }
-}
+
+
+
